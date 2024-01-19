283b2213d62e91320b959990238ffde2ba6444cf
susannakukka <susanna.kukkavuori@gmail.com>
2024-01-19 20:26:40 +0200

    fix legends

--- notebooks/plotting.py
+++ notebooks/plotting.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
 
-def init_plot(figsize=(10,6)):
-    fig, ax = plt.subplots(figsize=figsize)
+def init_plot(figsize=(8,4), constrained_layout=True):
+    fig, ax = plt.subplots(figsize=figsize, constrained_layout=constrained_layout)
     return fig, ax
